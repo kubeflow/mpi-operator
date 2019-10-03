721efa40d76666a7b79a1d8d74d499af7e0e7355
CJ Zheng <czheng94@bloomberg.net>
2019-10-03 11:24:25 -0400

    update status to running only when launcher is active and workers are all ready

--- pkg/controllers/v1alpha2/mpi_job_controller_status.go
+++ pkg/controllers/v1alpha2/mpi_job_controller_status.go
@@ -15,7 +15,7 @@
 package v1alpha2
 
 import (
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	common "github.com/kubeflow/common/job_controller/api/v1"
