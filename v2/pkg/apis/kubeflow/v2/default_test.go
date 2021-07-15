e80137c286388139e586f5f1bad542d0d0655e59
Aldo Culquicondor <1299064+alculquicondor@users.noreply.github.com>
2021-07-15 17:28:38 -0400

    Consolidate validation and defaulting logic (#376)
    Validation happens in a single place, improving coverage

---
+++ v2/pkg/apis/kubeflow/v2/default_test.go
@@ -0,0 +1,103 @@
+// Copyright 2021 The Kubeflow Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v2
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	common "github.com/kubeflow/common/pkg/apis/common/v1"
+)
+
+func TestSetDefaults_MPIJob(t *testing.T) {
+	cases := map[string]struct {
+		job  MPIJob
+		want MPIJob
+	}{
+		"base defaults": {
+			want: MPIJob{
+				Spec: MPIJobSpec{
+					SlotsPerWorker: newInt32(1),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyNone),
+				},
+			},
+		},
+		"base defaults overridden": {
+			job: MPIJob{
+				Spec: MPIJobSpec{
+					SlotsPerWorker: newInt32(10),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyRunning),
+				},
+			},
+			want: MPIJob{
+				Spec: MPIJobSpec{
+					SlotsPerWorker: newInt32(10),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyRunning),
+				},
+			},
+		},
+		"launcher defaults": {
+			job: MPIJob{
+				Spec: MPIJobSpec{
+					MPIReplicaSpecs: map[MPIReplicaType]*common.ReplicaSpec{
+						MPIReplicaTypeLauncher: {},
+					},
+				},
+			},
+			want: MPIJob{
+				Spec: MPIJobSpec{
+					SlotsPerWorker: newInt32(1),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyNone),
+					MPIReplicaSpecs: map[MPIReplicaType]*common.ReplicaSpec{
+						MPIReplicaTypeLauncher: {
+							Replicas:      newInt32(1),
+							RestartPolicy: DefaultRestartPolicy,
+						},
+					},
+				},
+			},
+		},
+		"worker defaults": {
+			job: MPIJob{
+				Spec: MPIJobSpec{
+					MPIReplicaSpecs: map[MPIReplicaType]*common.ReplicaSpec{
+						MPIReplicaTypeWorker: {},
+					},
+				},
+			},
+			want: MPIJob{
+				Spec: MPIJobSpec{
+					SlotsPerWorker: newInt32(1),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyNone),
+					MPIReplicaSpecs: map[MPIReplicaType]*common.ReplicaSpec{
+						MPIReplicaTypeWorker: {
+							Replicas:      newInt32(0),
+							RestartPolicy: DefaultRestartPolicy,
+						},
+					},
+				},
+			},
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := tc.job.DeepCopy()
+			SetDefaults_MPIJob(got)
+			if diff := cmp.Diff(tc.want, *got); diff != "" {
+				t.Errorf("Unexpected changes (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
---
+++ v2/pkg/apis/kubeflow/validation/validation_test.go
@@ -0,0 +1,155 @@
+// Copyright 2021 The Kubeflow Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validation
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	common "github.com/kubeflow/common/pkg/apis/common/v1"
+	"github.com/kubeflow/mpi-operator/v2/pkg/apis/kubeflow/v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+func TestValidateMPIJob(t *testing.T) {
+	cases := map[string]struct {
+		job      v2.MPIJob
+		wantErrs field.ErrorList
+	}{
+		"valid": {
+			job: v2.MPIJob{
+				Spec: v2.MPIJobSpec{
+					SlotsPerWorker: newInt32(2),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyRunning),
+					MPIReplicaSpecs: map[v2.MPIReplicaType]*common.ReplicaSpec{
+						v2.MPIReplicaTypeLauncher: {
+							Replicas: newInt32(1),
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{{}},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"valid with worker": {
+			job: v2.MPIJob{
+				Spec: v2.MPIJobSpec{
+					SlotsPerWorker: newInt32(2),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyRunning),
+					MPIReplicaSpecs: map[v2.MPIReplicaType]*common.ReplicaSpec{
+						v2.MPIReplicaTypeLauncher: {
+							Replicas: newInt32(1),
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{{}},
+								},
+							},
+						},
+						v2.MPIReplicaTypeWorker: {
+							Replicas: newInt32(3),
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{{}},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"empty job": {
+			wantErrs: field.ErrorList{
+				&field.Error{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.mpiReplicaSpecs",
+				},
+				&field.Error{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.slotsPerWorker",
+				},
+				&field.Error{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.cleanPodPolicy",
+				},
+			},
+		},
+		"empty replica specs": {
+			job: v2.MPIJob{
+				Spec: v2.MPIJobSpec{
+					SlotsPerWorker:  newInt32(2),
+					CleanPodPolicy:  newCleanPodPolicy(common.CleanPodPolicyRunning),
+					MPIReplicaSpecs: map[v2.MPIReplicaType]*common.ReplicaSpec{},
+				},
+			},
+			wantErrs: field.ErrorList{
+				&field.Error{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.mpiReplicaSpecs[Launcher]",
+				},
+			},
+		},
+		"missing replica spec fields": {
+			job: v2.MPIJob{
+				Spec: v2.MPIJobSpec{
+					SlotsPerWorker: newInt32(2),
+					CleanPodPolicy: newCleanPodPolicy(common.CleanPodPolicyRunning),
+					MPIReplicaSpecs: map[v2.MPIReplicaType]*common.ReplicaSpec{
+						v2.MPIReplicaTypeLauncher: {},
+						v2.MPIReplicaTypeWorker:   {},
+					},
+				},
+			},
+			wantErrs: field.ErrorList{
+				{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.mpiReplicaSpecs[Launcher].replicas",
+				},
+				{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.mpiReplicaSpecs[Launcher].template.spec.containers",
+				},
+				{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.mpiReplicaSpecs[Worker].replicas",
+				},
+				{
+					Type:  field.ErrorTypeRequired,
+					Field: "spec.mpiReplicaSpecs[Worker].template.spec.containers",
+				},
+			},
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := ValidateMPIJob(&tc.job)
+			if diff := cmp.Diff(tc.wantErrs, got, cmpopts.IgnoreFields(field.Error{}, "Detail", "BadValue")); diff != "" {
+				t.Errorf("Unexpected errors (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func newInt32(v int32) *int32 {
+	return &v
+}
+
+func newCleanPodPolicy(v common.CleanPodPolicy) *common.CleanPodPolicy {
+	return &v
+}
